f0bd32cbb40c5464bfb57278d096386ff533810e
Pablo Orviz <orviz@ifca.unican.es>
2024-11-19 13:10:35 +0100

    Add JePL file structure

--- .sqa/script.scummy-orchid-forest.sh
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
-#
-# SPDX-License-Identifier: GPL-3.0-only
-
-(
-cd gitlab.com/vmont/specfem3d &&
-    flake8 .
-)
